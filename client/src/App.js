5460b6e2cb9f550beef3473c11ef0b5e1cd0bb9c
asaudu <adaoraa.91@gmail.com>
2022-04-25 21:46:54 -0500

    have the pages rendering now

--- client/src/App.js
+++ client/src/App.js
@@ -14,12 +14,12 @@ return (
 	<Router>
 	<Navbar />
 	<Routes>
-		<Route path='/' exact component={Home} />
-		<Route path='/dallas' component={Dallas} />
-		<Route path='/korea' component={Korea} />
-    <Route path='/about' component={About} />
-		<Route path='/contactme' component={ContactMe} />
-		<Route path='/sign-up' component={SignUp} />
+		<Route path='/' exact element={<Home />} />
+		<Route path='/dallas' element={<Dallas />} />
+		<Route path='/korea' element={<Korea />} />
+    <Route path='/about' element={<About />} />
+		<Route path='/contactme' element={<ContactMe />} />
+		<Route path='/sign-up' element={<SignUp />} />
 	</Routes>
 	</Router>
 );
