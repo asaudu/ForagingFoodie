83db4a6ec3d292cb366fe1bc0809218f7f7f2b5b
asaudu <adaoraa.91@gmail.com>
2022-05-20 15:17:54 -0500

    styled single post views

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -20,28 +20,35 @@ const Home = () => {
   return (
     <div>
       <h1 className="homePageTitle">Welcome to the Foraging Foodie</h1>
-      <h2>Post of the Week</h2>
-      {posts.map((post) => (
-        <div key={post.id} className="card" style={{ width: "45rem" }}>
-          <img
-            className="card-img-top"
-            className="rounded-circle"
-            src={post.imageurl}
-            alt="Card image cap"
-            style={{ width: "600px", height: "500px" }}
-          />
-          <div className="card-body">
-            <h5 className="card-title">
-              {post.dish} from {post.restaurant}
-            </h5>
-            <p className="card-text-single">{post.content}</p>
 
-            {/* <button href="#" className="btn btn-primary">
-              Go somewhere
-            </button> */}
+      <div className="cardDiv" style={{ margin: "auto", width: "40rem" }}>
+        <h2>Post of the Week</h2>
+        {posts.map((post) => (
+          <div key={post.id} className="card">
+            <img
+              className="card-img-top"
+              className="rounded-circle"
+              src={post.imageurl}
+              alt="Card image cap"
+              style={{
+                width: "600px",
+                height: "500px",
+                paddingTop: "5px",
+                alignItems: "center",
+              }}
+            />
+            <div
+              className="card-body"
+              style={{ backgroundColor: "#084b83", borderRadius: "4px" }}
+            >
+              <h5 className="card-title">
+                {post.dish} from {post.restaurant}
+              </h5>
+              <p className="card-text-single">{post.content}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
